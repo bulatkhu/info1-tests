1348e2fe52db5b266d8ba58b361fcf757a7cec7c
Bulat Khudainazarov <hudainazarov.b@yandex.ru>
2022-11-13 19:13:29 +0100

    listPaths complete

--- src/listPaths/index.ts
+++ src/listPaths/index.ts
@@ -1,15 +1,16 @@
+type EdgesType = number[];
 // Bitte verändern Sie diese Klasse nicht!
 class CustomNode {
   color: string = 'red';
-  edges: number[] = [];
+  edges: EdgesType = [];
 
-  constructor(color: string, edges: number[]) {
+  constructor(color: string, edges: EdgesType) {
     this.color = color;
     this.edges = edges;
   }
 }
 
-const graph = [
+const graph: CustomNode[] = [
   new CustomNode('red', [1, 2, 4]),
   new CustomNode('white', [2]),
   new CustomNode('red', [3, 4]),
@@ -17,4 +18,23 @@ const graph = [
   new CustomNode('blue', []),
 ];
 
-console.log('graph', graph);
+function listPaths(graph: CustomNode[]) {
+  const values: EdgesType[] = [];
+  function valuesExtractor(edges: EdgesType, baseEdge: EdgesType) {
+    for (let i = 0; i < edges.length; i++) {
+      const edge = edges[i];
+      const mergedEdge = baseEdge.concat(edge);
+      values.push(mergedEdge);
+      valuesExtractor(graph[edge].edges, mergedEdge);
+    }
+  }
+  for (let index = 0; index < graph.length; index++) {
+    const { edges } = graph[index];
+    values.push([index]);
+    valuesExtractor(edges, [index]);
+  }
+  return values;
+}
+
+const values = listPaths(graph);
+console.log('values: ', values, 'values.length: ', values.length);
