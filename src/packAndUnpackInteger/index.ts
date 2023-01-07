7d5a1ab09ecad19d4d3f02f3e069564fcbc987d7
bukhudai <bulat.khudainazarov@digia.com>
2023-01-07 18:46:01 +0100

    better pack and unpack integer function

--- src/packAndUnpackInteger/index.ts
+++ src/packAndUnpackInteger/index.ts
@@ -1,20 +1,25 @@
+// number of bits per range
+const b0 = Math.ceil(Math.log2(59999));
+const b1 = Math.ceil(Math.log2(499));
+const b2 = Math.ceil(Math.log2(15));
+const b3 = Math.ceil(Math.log2(3));
+
 export function pack(a: number, b: number, c: number, d: number): number {
   // check parameter types and ranges
-  if (typeof a !== 'number' || typeof b !== 'number' || typeof c !== 'number' || typeof d !== 'number') {
-    throw "integer parameters expected";
+  if (
+    typeof a !== 'number' ||
+    typeof b !== 'number' ||
+    typeof c !== 'number' ||
+    typeof d !== 'number'
+  ) {
+    throw 'integer parameters expected';
   }
   if (a < 1 || b < 1 || c < 1 || d < 1) {
-    throw "parameter value out of range";
+    throw 'parameter value out of range';
   }
   if (a > 60000 || b > 500 || c > 16 || d > 4) {
-    throw "parameter value out of range";
+    throw 'parameter value out of range';
   }
-
-  // number of bits per range
-  const b0 = Math.ceil(Math.log2(59999));
-  const b1 = Math.ceil(Math.log2(499));
-  const b2 = Math.ceil(Math.log2(15));
-  const b3 = Math.ceil(Math.log2(3));
 
   // number of bits to shift
   const s0 = 0;
@@ -23,20 +28,19 @@ export function pack(a: number, b: number, c: number, d: number): number {
   const s3 = s2 + b2;
 
   // pack the numbers
-  return (2**s0 * (a-1)) | (2**s1 * (b-1)) | (2**s2 * (c-1)) | (2**s3 * (d-1));
+  return (
+    (2 ** s0 * (a - 1)) |
+    (2 ** s1 * (b - 1)) |
+    (2 ** s2 * (c - 1)) |
+    (2 ** s3 * (d - 1))
+  );
 }
 
 export function unpack(n: number): number[] {
   // check parameter type
   if (typeof n !== 'number') {
-    throw "integer parameter expected";
+    throw 'integer parameter expected';
   }
-
-  // number of bits per range
-  const b0 = Math.ceil(Math.log2(59999));
-  const b1 = Math.ceil(Math.log2(499));
-  const b2 = Math.ceil(Math.log2(15));
-  const b3 = Math.ceil(Math.log2(3));
 
   // number of bits to shift
   const s0 = 0;
@@ -45,9 +49,9 @@ export function unpack(n: number): number[] {
   const s3 = s2 + b2;
 
   // unpack the number
-  const a = (n >>> s0) & (2**b0 - 1);
-  const b = (n >>> s1) & (2**b1 - 1);
-  const c = (n >>> s2) & (2**b2 - 1);
-  const d = (n >>> s3) & (2**b3 - 1);
-  return [a+1, b+1, c+1, d+1];
+  const a = (n >>> s0) & (2 ** b0 - 1);
+  const b = (n >>> s1) & (2 ** b1 - 1);
+  const c = (n >>> s2) & (2 ** b2 - 1);
+  const d = (n >>> s3) & (2 ** b3 - 1);
+  return [a + 1, b + 1, c + 1, d + 1];
 }
