06c9bb11612fb1e3ea78cadadf576d9645384081
bukhudai <bulat.khudainazarov@digia.com>
2023-01-21 11:01:54 +0100

    calculate time complexity #3

--- src/O-Notation/index.ts
+++ src/O-Notation/index.ts
@@ -14,24 +14,39 @@ function ONotation(n: number) {
   } // 0(n^2)
 } // result: Max(O(1), O(n), O(n^2)) => O(n^2)
 
-// function function1(n: number) {
-//   return (2 + 1 / n) ** n;
-// }
-//
-// function function2(n: number) {
-//   return n ** 2 + n ** 2 / (n * Math.log(n)) / (Math.sqrt(n) + 1);
-// }
-//
-// function function3(n: number) {
-//   return n ** (n - 1) / (n ** 7 + 10);
-// }
-//
-// function function4(n: number) {
-//   return n ** 2 / n / (Math.sqrt(n) + 3);
-// }
-//
-// function function5(n: number) {
-//   return n + 3 ** (n ** 2 / (n + 5));
-// }
+function measurePerformance<T, R>(callback: (value: T) => R, value: T) {
+  const start = performance.now();
+  callback(value);
+  const end = performance.now();
+  return end - start;
+}
 
-// console.log('result', function1(2));
+function function1(n: number) {
+  return (2 + 1 / n) ** n;
+}
+
+function function2(n: number) {
+  return n ** 2 + n ** 2 / (n * Math.log(n)) / (Math.sqrt(n) + 1);
+}
+
+function function3(n: number) {
+  return n ** (n - 1) / (n ** 7 + 10);
+}
+
+function function4(n: number) {
+  return n ** 2 / n / (Math.sqrt(n) + 3);
+}
+
+function function5(n: number) {
+  return n + 3 ** (n ** 2 / (n + 5));
+}
+
+const results = [
+  measurePerformance(function5, 1000000000000000000000000000000000000000),
+  measurePerformance(function1, 1000000000000000000000000000000000000000),
+  measurePerformance(function3, 1000000000000000000000000000000000000000),
+  measurePerformance(function4, 1000000000000000000000000000000000000000),
+  measurePerformance(function2, 1000000000000000000000000000000000000000),
+];
+
+console.log('results', results);
